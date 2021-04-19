7bd651bf52dd5db56ae4e04c807b0616375bcaa4
Ashik Rahman <81816852+ashik5757@users.noreply.github.com>
2021-04-19 22:25:53 +0600

    Basic C

---
+++ LAB_test/LAB01/Absolute Value/Absolute_value.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+
+{
+    int num;
+    double a;
+    
+    printf("Enter the value :");
+    scanf("%d",&num);
+    
+    a=fabs(num);
+    
+ printf("\n The absolute value of '%d' is : %.2f",num,a);
+    
+    
+    
+    return 0;
+}
