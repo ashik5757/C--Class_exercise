962d28b2901126860b07d1c480f8690b087a36ac
Ashik Rahman <81816852+ashik5757@users.noreply.github.com>
2021-04-20 00:57:56 +0600

    Basic C

---
+++ LAB_test/LAB01/Print_Floating_Point/float_3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    float x,a;
+    int i;
+    
+    
+    printf(" Enter a floating number :");
+    
+   scanf("%f",&x);
+
+    i=x;
+    
+    a=x-i;
+    
+  printf("\n Without integer part value is :%.3f",a);
+    
+    
+ 
+    
+    
+    
+    return 0;
+}
