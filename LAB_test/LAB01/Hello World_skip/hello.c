c60148d5a10a763739a171ba067327477d69286e
Ashik Rahman <81816852+ashik5757@users.noreply.github.com>
2021-04-20 00:49:51 +0600

    Basic C

---
+++ LAB_test/LAB01/Hello World_skip/hello.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=1;
+    
+    while(i<=10) {
+        
+      
+      
+        
+        
+        printf(" %d Hello World \n",i);
+        
+        i++;
+        
+        
+        if(i==7){
+         
+               i++;
+           
+        }
+        
+        
+    }
+    
+ 
+    
+    
+    
+    return 0;
+}
