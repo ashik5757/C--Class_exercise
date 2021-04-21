76ab2ef8e83ab3a96015c8573ed42654c21dd65a
Ashik Rahman <81816852+ashik5757@users.noreply.github.com>
2021-04-21 17:52:13 +0600

    Loop

---
+++ LAB_test/LAB_02/Summation of all digits/sum_digits.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int i,n,m,r;
+    int x=0;
+
+
+    printf(" Enter an integer : ");
+    scanf("%d",&n);
+
+    while(n!=0) {
+
+        r=n%10;
+        x+=r;
+        m=n-r;
+        n=m/10;
+
+    }
+    
+    printf("\n Ans: %d",x);
+    
+    
+    
+    
+    return 0;
+}
