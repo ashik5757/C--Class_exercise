23dd1ce8e198da423e64db2f569dd390cf5d9407
Ashik Rahman <81816852+ashik5757@users.noreply.github.com>
2021-04-21 18:27:41 +0600

    Loop & Statement

---
+++ LAB_test/LAB_02/factorial/Fact.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int num;
+    int i,x=1;
+   
+    printf(" Enter a non-negative integer : ");
+   
+    scanf("%d",&num);
+    
+    if(num>=0) {
+    	
+    for(i=1;i<=num;i++){
+        
+        x=x*i;
+        
+    }	
+    	
+    	printf("\n  Factorial : %d",x);
+    	
+    	
+    	
+	}
+    
+    else {
+    	
+    	printf("\n This is not a non-negative integer ");
+    	
+	}
+    
+    
+    
+    
+    return 0;
+}
